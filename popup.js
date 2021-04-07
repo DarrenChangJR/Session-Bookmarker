c94367bad7dc8383a636a72888b7e40b4cdec654
DarrenChangJR <48514988+DarrenChangJR@users.noreply.github.com>
2021-04-08 00:43:50 +0800

    Added Rename Session function

--- popup.js
+++ popup.js
@@ -16,14 +16,18 @@ function addhtml() {
   var sessionName = window.prompt("Name of session:", "Leaf");
   if (sessionName !== null) {
     let bigButt = document.createElement("button");
+    let renameButt = document.createElement("button");
     let smallButt = document.createElement("button");
     bigButt.setAttribute("class", "big-button");
+    renameButt.setAttribute("class", "rename-button");
     smallButt.setAttribute("class", "small-button");
     bigButt.setAttribute("id", "big"+getCounter());
+    renameButt.setAttribute("id", "rename"+getCounter());
     smallButt.setAttribute("id", "small"+getCounter());
     bigButt.textContent = sessionName;
+    renameButt.textContent = "Rename";
     smallButt.textContent = "X";
-    document.querySelector("#sessions-chunk").append(bigButt, smallButt);
+    document.querySelector("#sessions-chunk").append(bigButt, renameButt, smallButt);
     localStorage.setItem("htmlchunk", document.querySelector("#sessions-chunk").outerHTML);
     return true;
   } else {
@@ -45,6 +49,16 @@ function deleteSession() {
     localStorage.removeItem(num);
     document.querySelector(`[id=big${CSS.escape(num)}]`).remove();
     document.querySelector(`[id=small${CSS.escape(num)}]`).remove();
+    document.querySelector(`[id=rename${CSS.escape(num)}]`).remove();
+    localStorage.setItem("htmlchunk", document.querySelector("#sessions-chunk").outerHTML);
+  };
+}
+
+function renameSession() {
+  let num = this.id.split("rename")[1];
+  var newName = window.prompt("Rename session to:", "Flower");
+  if (newName !== null) {
+    document.querySelector("#big"+num).textContent = newName;
     localStorage.setItem("htmlchunk", document.querySelector("#sessions-chunk").outerHTML);
   };
 }
@@ -52,9 +66,11 @@ function deleteSession() {
 function clickAttacher() {
   var arrBig = document.querySelectorAll(".big-button");
   var arrSmall = document.querySelectorAll(".small-button");
+  var arrRename = document.querySelectorAll(".rename-button");
   for (var i = 0; i<arrBig.length; i++) {
     arrBig[i].addEventListener("click", reopenSession);
     arrSmall[i].addEventListener("click", deleteSession);
+    arrRename[i].addEventListener("click", renameSession)
   };
 }
 
